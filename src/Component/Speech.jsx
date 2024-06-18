4dd269fbb8cdacbf57385e2df6111ae32884f597
ashan <uhashan17929@gmail.com>
2024-06-18 19:18:43 +0530

    Set a Responsiveness

--- src/Component/Speech.jsx
+++ src/Component/Speech.jsx
@@ -34,33 +34,32 @@ const Speech = () => {
     }
 
     return (
-        <div>
+        <div className='container mx-auto p-4'>
             <div>
-                <h2 className='text-black text-center text-[48px] leading-5 mt-8'>Speech to text Converter</h2><br />
-                <p className='max-w-[50rem] text-center mt-[24px] mb-[50px]'>
+                <h2 className='text-black text-center text-3xl md:text-4xl lg:text-5xl leading-5 mt-8 '>Speech to Text Converter</h2><br />
+                <p className='max-w-3xl mx-auto text-center mt-6 mb-12 text-sm md:text-base lg:text-lg'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nemo eos perspiciatis ab placeat molestias, vero qui. Architecto, cumque impedit quo dolores quam perferendis accusantium excepturi perspiciatis dicta obcaecati ducimus.
                 </p>
 
                 <textarea
-                    className='text-start text-[16px] max-w-[50rem] w-[100%] min-h-[400px] h-auto py-[18px] px-[18px] bg-slate-300 hover:bg-[#b7f3eb] shadow-black border-[1px] rounded-[20px] relative'
+                    className='text-start text-base max-w-3xl w-full min-h-[200px] md:min-h-[300px] lg:min-h-[400px] h-auto py-4 px-4 bg-slate-300 hover:bg-[#b7f3eb] shadow-black border rounded-lg relative mx-auto'
                     value={textToCopy}
                     onChange={handleTextChange}
                 ></textarea>
 
-                <div className='flex gap-10 p-10 justify-center items-center'>
-                    
-                    <button className='bg-emerald-400 text-black border-none rounded-lg py-[18px] px-[30px] text-[18px] leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={startListening}>
+                <div className='flex flex-col md:flex-row gap-4 p-4 justify-center items-center mt-6'>
+                    <button className='bg-emerald-400 text-black border-none rounded-lg py-2 px-4 text-sm md:text-base lg:text-lg leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={startListening}>
                         Start Listening
                     </button>
-                    <button className='bg-emerald-400 text-black border-none rounded-lg py-[18px] px-[30px] text-[18px] leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={SpeechRecognition.stopListening}>
+                    <button className='bg-emerald-400 text-black border-none rounded-lg py-2 px-4 text-sm md:text-base lg:text-lg leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={SpeechRecognition.stopListening}>
                         Stop Listening
                     </button>
 
-                    <button className='bg-emerald-400 text-black border-none rounded-lg py-[18px] px-[40px] text-[18px] leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={setCopied}>
+                    <button className='bg-emerald-400 text-black border-none rounded-lg py-2 px-4 text-sm md:text-base lg:text-lg leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={setCopied}>
                         {isCopied ? 'Copied!' : 'Copy'}
                     </button>
 
-                    <button className='bg-emerald-400 text-black border-none rounded-lg py-[18px] px-[40px] text-[18px] leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={clearText}>
+                    <button className='bg-emerald-400 text-black border-none rounded-lg py-2 px-4 text-sm md:text-base lg:text-lg leading-4 relative cursor-pointer focus:bg-[#73db5f] hover:bg-[#82f16c]' onClick={clearText}>
                         Clear
                     </button>
                 </div>
